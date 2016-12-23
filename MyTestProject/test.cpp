7691db71ebd9b6547bae0bb18f4f3032af3fb88b
mkang_hyd <mkang_hyd@163.com>
2016-12-23 16:47:33 +0800

    Revert "不相交集类"
    This reverts commit a18630b7601fa1033b123410866253524d043a7c.

--- MyTestProject/test.cpp
+++ MyTestProject/test.cpp
@@ -24,48 +24,78 @@
 // constants
 using namespace std;
 // function prototype
-class DisjSets
+void BucketSortSimple(vector<int> &a)
 {
-public:
-	explicit DisjSets(int numElements);
-	int find(int x) const;
-	int find(int x);
-	void unionSets(int root1, int root2);
+	vector<int> bucket(a.size(), 0);
+	for (int i = 0; i < a.size(); i++)
+		bucket[a[i]]++;
+	int index = 0;
+	for (int i = 0; i < a.size(); i++)
+		for (int j = 1; j <= bucket[i]; j++)
+			a[index++] = i;
+}
+template<class T>
+class BucketSort
+{
 private:
-	vector<int> s;
+	struct ListNode
+	{
+		T element;
+		ListNode *next;
+		ListNode(const T &theElement = 0, ListNode *nextnode = nullptr)
+			:element(theElement), next(nextnode) {}
+	};
+	int BucketSize;
+	ListNode *Bucket;
+public:
+	BucketSort(int size = 10)
+	{
+		BucketSize = size;
+		Bucket = new ListNode[size];
+	}
+	void sort(vector<T> &a)
+	{
+		int BucketIndex;
+		ListNode *p, *q;
+		int count = 0;
+		for (int i = 0; i < a.size(); i++)
+		{
+			ListNode *insert = new ListNode(a[i], nullptr);
+			BucketIndex = a[i] * 1;
+			if (Bucket[BucketIndex].next == nullptr)
+				Bucket[BucketIndex].next = insert;
+			else
+			{
+				p = &Bucket[BucketIndex];
+				q = p->next;
+				while (q != nullptr&&q->element < a[i])
+				{
+					p = q;
+					q = p->next;
+				}
+				insert->next = q;
+				p->next = insert;
+			}
+		}
+		for (int i = 0; i < BucketSize; i++)
+		{
+			p = Bucket[i].next;
+			if (p == nullptr)
+				continue;
+			while (p != nullptr)
+			{
+				a[count++] = p->element;
+				p = p->next;
+			}
+		}
+	}
 };
-DisjSets::DisjSets(int numElements)
-{
-	for (int i = 0; i < s.size(); i++)
-		s[i] = -1;
-}
-
-int DisjSets::find(int x) const
-{
-	if (s[x] < 0)
-		return x;
-	else
-		return find(s[x]);
-}
-
-int DisjSets::find(int x)
-{
-	if (s[x] < 0)
-		return x;
-	else
-		return find(s[x]);
-}
-
-void DisjSets::unionSets(int root1, int root2)
-{
-	s[root2] = root1;
-}
-
 int main()
 {
 	vector<int> test({ 10,9,8,7,6,5,4,3,2,1,0 });
 	vector<double> test2({ 0.78, 0.17, 0.39, 0.26, 0.72, 0.34, 0.94, 0.21, 0.12, 0.23 });
-	
+	BucketSort<int> bucketsort(11);
+	bucketsort.sort(test);
 	cout << log2(320) << endl;
 	/*BucketSortSimple(test);*/
 	for (auto x : test)
