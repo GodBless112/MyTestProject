4fff98ba36e33bf4b83e303822f942b88aa49508
mkang_hyd <mkang_hyd@163.com>
2017-02-21 19:23:44 +0800

    Prim算法

--- MyTestProject/test.cpp
+++ MyTestProject/test.cpp
@@ -1,10 +1,12 @@
-﻿///桶排序
-//线性时间排序，时间复杂度为O(n)=n+(nlogn-nlogr)，r为桶的大小
+﻿///Dijkstra算法
+//贪心算法，将所有点集分为已知和未知两部分，初始化选择一个点开始，然后每次选择
+//未知集中离起点距离最近的点，并更新未知集中的距离信息，直到没有未知点为止。
 #include<iostream>
 #include<fstream>
 #include<sstream>
 #include<string>
 #include<cmath>
+#include<ctime>
 #include<iomanip>
 #include<exception>
 #include<cstdlib>
@@ -14,9 +16,11 @@
 #include<map>
 #include<vector>
 #include<stack>
+#include<list>
 #include<queue>
 #include<algorithm>
 #include<functional>
+#include<numeric>
 #include<iterator>
 #include<unordered_map>
 #include<Windows.h>
@@ -24,54 +28,120 @@
 // constants
 using namespace std;
 // function prototype
-class DisjSets
+//伪代码
+namespace Graph
 {
-public:
-	explicit DisjSets(int numElements);
-	int find(int x) const;
-	int find(int x);
-	void unionSets(int root1, int root2);
-private:
-	vector<int> s;
-};
-DisjSets::DisjSets(int numElements)
-{
-	for (int i = 0; i < s.size(); i++)
-		s[i] = -1;
+#define DistType int
+	struct Vertex
+	{
+		vector<Vertex *> adj;	//连接点的列表
+		bool known;			//路径是否已知
+		DistType dist;		//路径代价
+		Vertex *path;		//存储路径
+		int index;
+	};
+	vector<Vertex> s;
+	int **c;
+	void printPath(Vertex *v);
+	void dijkstra(Vertex *v);
+	Vertex* findSmallUnknownDist(vector<Vertex> &rhs);
 }
-
-int DisjSets::find(int x) const
+using Graph::Vertex;
+void Graph::printPath(Vertex *v)
 {
-	if (s[x] < 0)
-		return x;
-	else
-		return find(s[x]);
+	if (v->path != nullptr)
+	{
+		printPath(v->path);
+		cout << " to ";
+	}
+	cout << v->index;
 }
-
-int DisjSets::find(int x)
+void Graph::dijkstra(Vertex *v)
 {
-	if (s[x] < 0)
-		return x;
-	else
-		return find(s[x]);
+	int index = 0;
+	for (auto &x : s)
+	{
+		x.dist = 999999;
+		x.known = false;
+		x.index = index++;
+	}
+	v->dist = 0;
+	for (;;)
+	{
+		Vertex* vv = findSmallUnknownDist(s);
+		if (vv == nullptr)
+			break;
+		vv->known = true;
+		for (int i = 0; i < vv->adj.size(); i++)
+		{
+			Vertex* w = vv->adj[i];
+			if (!w->known)
+				if ((vv->dist + c[vv->index][w->index])< w->dist)
+				{
+					w->dist = vv->dist + c[vv->index][w->index];
+					w->path = vv;
+				}
+		}
+	}
 }
-
-void DisjSets::unionSets(int root1, int root2)
+Vertex* Graph::findSmallUnknownDist(vector<Vertex>& rhs)
 {
-	s[root2] = root1;
+	Vertex* result = nullptr;
+	DistType mindist = 999999;
+	for (auto &v : rhs)
+	{
+		if (v.known == false && v.dist < mindist)
+		{
+			result = &v;
+			mindist = v.dist;
+		}
+	}
+	return result;
 }
-
 int main()
 {
-	vector<int> test({ 10,9,8,7,6,5,4,3,2,1,0 });
-	vector<double> test2({ 0.78, 0.17, 0.39, 0.26, 0.72, 0.34, 0.94, 0.21, 0.12, 0.23 });
-	BucketSort<int> bucketsort(11);
-	bucketsort.sort(test);
-	cout << log2(320) << endl;
-	/*BucketSortSimple(test);*/
-	for (auto x : test)
-		cout << x << " ";
+	using Graph::s;
+	using Graph::c;
+	s.resize(7);
+	s[0].adj.push_back(&s[1]);
+	s[0].adj.push_back(&s[3]);
+	s[1].adj.push_back(&s[3]);
+	s[1].adj.push_back(&s[4]);
+	s[2].adj.push_back(&s[0]);
+	s[2].adj.push_back(&s[5]);
+	s[3].adj.push_back(&s[4]);
+	s[3].adj.push_back(&s[5]);
+	s[3].adj.push_back(&s[6]);
+	s[3].adj.push_back(&s[2]);
+	s[4].adj.push_back(&s[6]);
+	s[6].adj.push_back(&s[5]);
+	c = new int*[7];
+	for (int i = 0; i < 7; i++)
+		c[i] = new int[6];
+	c[0][0] = 0; c[0][1] = 2; c[0][2] = 999999; c[0][3] = 1; c[0][4] = 999999; c[0][5] = 999999; c[0][6] = 999999;
+	c[1][0] = 999999; c[1][1] = 0; c[1][2] = 999999; c[1][3] = 3; c[1][4] = 10; c[1][5] = 999999; c[1][6] = 999999;
+	c[2][0] = 4; c[2][1] = 999999; c[2][2] = 0; c[2][3] = 999999; c[2][4] = 999999; c[2][5] = 5; c[2][6] = 999999;
+	c[3][0] = 999999; c[3][1] = 999999; c[3][2] = 2; c[3][3] = 0; c[3][4] = 2; c[3][5] = 8; c[3][6] = 4;
+	c[4][0] = 999999; c[4][1] = 999999; c[4][2] = 999999; c[4][3] = 999999; c[4][4] = 0; c[4][5] = 999999; c[4][6] = 6;
+	c[5][0] = 999999; c[5][1] = 999999; c[5][2] = 999999; c[5][3] = 999999; c[5][4] = 999999; c[5][5] = 0; c[5][6] = 999999;
+	c[6][0] = 999999; c[6][1] = 999999; c[6][2] = 999999; c[6][3] = 999999; c[6][4] = 999999; c[6][5] = 1; c[6][6] = 0;
+	vector<int> test(10);
+	for (auto &x : test)
+		x = 1;
+	Graph::dijkstra(&s[0]);
+	Graph::printPath(&s[1]);
 	cout << endl;
+	Graph::printPath(&s[2]);
+	cout << endl;
+	Graph::printPath(&s[3]);
+	cout << endl;
+	Graph::printPath(&s[4]);
+	cout << endl;
+	Graph::printPath(&s[5]);
+	cout << endl;
+	Graph::printPath(&s[6]);
+	cout << endl;
+
 	cout << "Done.\n";
 	// code to keep window open for MSVC++
 	std::cin.clear();
@@ -80,4 +150,5 @@ int main()
 	std::cin.get();
 	return 0;
 }
+
 
